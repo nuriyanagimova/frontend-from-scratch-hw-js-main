6b54b6b84f0408659a0368c499e64a8cbc66260a
NagimovaN <nagimovanuriya@gmil.com>
2024-11-27 11:24:57 +0100

    3 exos

--- lesson-03/01.js
+++ lesson-03/01.js
@@ -1,3 +1,8 @@
 // Напишите функцию calculateAverage, которая принимает 2 числа и возвращает их среднее арифметическое
 
-function calculateAverage() {}
+function calculateAverage(a, b) {
+    return(a+b)/2
+}
+const result = calculateAverage(2, 4)
+ 
+console.log(result)
--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,4 +1,9 @@
 // Разработайте функцию isEven, которая определяет, чётное ли число.
 // Функция должна возвращать true, если число чётное, и false — в противном случае
 
-function isEven() {}
+function isEven(num) {
+   return num%2===0 
+}
+
+const result = isEven(3)
+console.log(result) 
